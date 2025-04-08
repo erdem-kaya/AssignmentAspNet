ac4688abf0a06f4ba66ae43e44c532786516d927
Erdem Kaya <kerdem@live.com>
2025-04-08 18:16:30 +0200

    new js for quill editor.

---
+++ WebApp/wwwroot/js/quill-editor.js
@@ -0,0 +1,19 @@
+﻿// Jag har byggt den med Hans's video. Rich Text Editor 
+function initWysiwyg(wysiwygEditorId, wysiwygToolbarId, textareId, content) {
+    const textarea = document.querySelector(textareId)
+    const quill = new Quill(wysiwygEditorId, {
+        modules: {
+            syntax: true,
+            toolbar: wysiwygToolbarId
+        },
+        placeholder: 'Type something',
+        theme: 'snow'
+    })
+
+    if (content)
+        quill.root.innerHTML = content;
+
+    quill.on('text-change', () => {
+        textarea.value = quill.root.innerHTML;
+    });
+}
